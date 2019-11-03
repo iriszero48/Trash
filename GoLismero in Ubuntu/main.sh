e5de240f26847b633e5699a5308807e201c061fe
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-03 19:12:09 +0800

    Create main.sh

---
+++ GoLismero in Ubuntu/main.sh
@@ -0,0 +1,7 @@
+apt-get install python2.7 python2.7-dev python-pip python-docutils git perl nmap sslscan
+cd /opt
+git clone https://github.com/golismero/golismero.git
+cd golismero
+pip install -r requirements.txt
+pip install -r requirements_unix.txt
+ln -s /opt/golismero/golismero.py /usr/bin/golismero
