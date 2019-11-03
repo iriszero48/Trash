65b96141195cca04feb362b6e947942d1e927889
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-03 14:47:05 +0800

    Create main.cs

---
+++ Solutions by Programming Task/Binary digits/main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SolutionsByProgrammingTask
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = 9000;
+            Console.WriteLine(Convert.ToString(x,2));
+        }
+    }
+}
