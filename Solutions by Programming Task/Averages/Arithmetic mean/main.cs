0a66204baf22cbb86c5db8a58ffdc180d0ce645f
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-03 14:45:41 +0800

    Create main.cs

---
+++ Solutions by Programming Task/Averages/Arithmetic mean/main.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+
+namespace SolutionsByProgrammingTask
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = {1, 2, 3};
+            var mean = a.Average();
+        }
+    }
+}
