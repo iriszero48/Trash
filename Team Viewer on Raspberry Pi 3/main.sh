9cc4549a2de2641cb05a341936953d197ebfbcf0
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-04 09:09:41 +0800

    Create main.sh

---
+++ Team Viewer on Raspberry Pi 3/main.sh
@@ -0,0 +1,5 @@
+wget http://download.teamviewer.com/download/linux/version_11x/teamviewer-host_armhf.deb
+sudo dpkg -i teamviewer-host_armhf.deb
+sudo apt-get -f install
+sudo apt-get install gdebi
+teamviewer info
