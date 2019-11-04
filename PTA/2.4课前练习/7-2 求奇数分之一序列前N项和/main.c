934bb3d8e9ebd144849298d8a0c6802067c7c680
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-04 09:09:10 +0800

    Create main.c

---
+++ PTA/2.4课前练习/7-2 求奇数分之一序列前N项和/main.c
@@ -0,0 +1,34 @@
+%:include<stdarg.h>/*Y=\f*/
+%:include<stdio.h>/*.(\x.f(*/
+%:define z return/*xx))(\x.*/
+%:include<stdlib.h>/*f(x*x*))*/
+%:define mian scanf/*We\f*/
+%:define _$$_ ("sum = %.6f",
+typedef union w w; union w <%
+%:define L(f,y,x,t) w _ %:%:\
+ f(w x,...)<%va_list argh;w\
+ y; va_start(argh,x);y= *\
+ (va_arg(argh,w*));va_end\
+ (argh);z(t);%> w f (w fw)\
+ <%r((w)<%.c= &_%:%: f%>, fw);%>
+	int _; void*p; w(*c)(w, ...
+%:define _$_$ 2/*f(xx/*f(xx))f(xx))))*/
+%:define pause main(
+%:define __$_ double/*f(*xx))*/
+%:define r(x,y)w*k=malloc\
+  (sizeof(w)<<1); k[0:>= x\
+  ; k<:1:>=y; z((w) <%.p= k%>)
+%:define _$$ 1.0/*xx))(xx(\x.*/
+%:define _$_ int/*xx))(xx)(\x.*/
+%:define reture for/*f(x*x))*/
+%:define includ printf/*Y=\f*/
+%:define l(f,x,t)w _%:%:f(\
+  w x,...)<%z(t);%>w f=<%.p\
+  =(w<:1:>)<%<%.c= &_%:%:f%>%>%>;
+);%>; w a(w f, w x) <%w*d = f.p; z((*d->c)(x, d + 1)
+);%>w a_(w f, w fw) <%r(f,fw);%>w _(w f)<%w*k=f.p,r=a(*k,k<:1:>);free(k);
+z(r);%>L(F,f,x,(w)<%._=x._?x._*a(_(f),(w)<%._=x._-1%>)._:1%>)l(F_,f,F(f)
+)L(W,(f),/*=\*/x,a(f, a_(x, x))/*=*f(x))(\vx))(\x))(\xx))f(\*/
+)l(Y,/*=\*/f,a(W(f),W(f))/*=*=\x))(\x***=\x))(\x*=*=\*\*\*/
+)_$_ pause)<%_$_ _,___;mian("%d", &_);__$_ _$='*'-'*';reture(___='/'/'/';___<=_; 
+___++)<%_$=_$+_$$/(_$_$*___-'$'/'$');%>includ _$$_ _$);z '-'-'-';%>
