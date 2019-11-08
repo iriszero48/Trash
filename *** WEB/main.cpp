67f14f3e6bd436f17072f394fc1e6ea28d2d8e41
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-09 01:43:55 +0800

    Update main.cpp

--- *** WEB/main.cpp
+++ *** WEB/main.cpp
@@ -4,11 +4,26 @@
 #include <algorithm>
 #include <regex>
 using namespace std;
+
 int main(int argc, char* argv[])
-{
+{/*
+	system("python3 GetUrl.py");
+	ifstream fr1;
+	fr1.open("weburl.txt");
+	vector<string> urlList;
+	string temp;
+	while (getline(fr1, temp)) urlList.push_back(temp);
+	fr1.close();
+	ifstream fr2;
+	fr2.open("fuck.txt");
+	vector<string> fuckList;
+	while (getline(fr2,temp))fuckList.push_back(temp);
+	fr2.close();*/
+	system("python3 hb.py&");
+	//system("python3 fix.py&");
 	while (true)
 	{
-		system("python3 GetUrl.py");
+	system("python3 GetUrl.py");
         ifstream fr1;
         fr1.open("weburl.txt");
         vector<string> urlList;
@@ -20,12 +35,15 @@ int main(int argc, char* argv[])
         vector<string> fuckList;
         while (getline(fr2,temp))fuckList.push_back(temp);
         fr2.close();
+	system("python3 fix.py&");
+
 		for_each(fuckList.begin(), fuckList.end(), [&](string& fuck)
 		{
 			for_each(urlList.begin(), urlList.end(), [&](string& url)
 			{
-				system((regex_replace(fuck, regex("\\$url\\$"), url)).c_str());
+				system((regex_replace(fuck, regex("\\$url\\$"), url) + "&").c_str());
 			});
 		});
+		system("sleep 5");
 	}
 }
