c781b52c0d6f44a4c0fef9476b9398609339c8d0
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-11-21 12:00:03 +0800

    Create main.fs

---
+++ Solutions by Programming Task/Closest-pair problem/F#/main.fs
@@ -0,0 +1,8 @@
+let ClosestPairs (points:Point []) =
+    let n = points.Length - 1
+    in seq {
+        for i in 0..n-1 do
+            for j in i+1..n do
+                yield points.[i], points.[j]
+    }
+    |> Seq.minBy (fun (a, b) -> (b - a).LengthSquared)
