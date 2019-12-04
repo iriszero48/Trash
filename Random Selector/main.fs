20e56a82b4b860ebd2715d1ee701dc53248563e0
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-12-04 11:53:56 +0800

    Update main.fs

--- Random Selector/main.fs
+++ Random Selector/main.fs
@@ -4,6 +4,7 @@ open System
 open Microsoft.WindowsAPICodePack.Taskbar
 open System.Drawing
 open System.Diagnostics
+open System.Security.Cryptography
 
 let (|>|>) x f = f x |> ignore; x
 
@@ -11,9 +12,11 @@ let (|>|>) x f = f x |> ignore; x
 let main argv = 
     match argv.Length with
     | 1 ->
+        let rng = new RNGCryptoServiceProvider()
+        let arr = Array.create 8 0uy
         argv.[0]
         |> Directory.GetFiles
-        |> (fun x -> x.OrderBy(fun _ -> Guid.NewGuid()).ToList().[0])
+        |> (fun x -> x.OrderBy(fun _ -> rng.GetBytes(arr); BitConverter.ToUInt64(arr, 0)).ToList().[0])
         |>|> (fun x -> TaskbarManager.Instance.SetOverlayIcon(Icon.ExtractAssociatedIcon(x), ""))
         |>|> Console.WriteLine
         |>|> (fun _ -> Console.ReadLine())
