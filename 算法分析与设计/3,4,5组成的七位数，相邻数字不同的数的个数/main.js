fc2dc5fd3b68be2ab30bde42c6ec732e34ee202c
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-06-18 01:56:07 +0800

    Update main.js

--- 算法分析与设计/3,4,5组成的七位数，相邻数字不同的数的个数/main.js
+++ 算法分析与设计/3,4,5组成的七位数，相邻数字不同的数的个数/main.js
@@ -1,1 +1,6 @@
-Array(7).fill([...Array(3).keys()].map(x => (x + 3).toString())).reduce((a, b) => a.flatMap(x => b.map(y => x + y))).filter(x => !/(\d)\1/.test(x)).map(Number)
+Array(7)
+        .fill([...Array(3).keys()].map(x => (x + 3).toString()))
+        .reduce((a, b) => a.flatMap(x => b.map(y => x + y)))
+        .filter(x => !/(\d)\1/.test(x))
+        .map(Number)
+        .length
