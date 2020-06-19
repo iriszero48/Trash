07c974f8b519aaa9b6900a478a563eb311125a57
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-06-19 22:07:33 +0800

    Update main.js

--- 算法分析与设计/画*/main.js
+++ 算法分析与设计/画*/main.js
@@ -1,7 +1,6 @@
 require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin
 }).on('line', (line) => {
-    const nm = +line / 2 + 1;
-    [...Array(+line + 2).keys()].slice(1).forEach(x => console.log(" ".repeat(Math.abs(nm - x)) + "*".repeat(2*(nm - Math.abs(nm - x))-1)));
+    const nm = Math.floor(+line / 2 + 1);
+    [...Array(+line + 1).keys()].slice(1).forEach(x => console.log(" ".repeat(Math.abs(nm - x)) + "*".repeat(2 * (nm - Math.abs(nm - x)) - 1)));
 });
