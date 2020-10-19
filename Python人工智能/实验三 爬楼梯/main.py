c15394a89e798e84e2a297537523b8bdcdc5dbf9
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-10-20 02:37:16 +0800

    Create main.py

---
+++ Python人工智能/实验三 爬楼梯/main.py
@@ -0,0 +1,5 @@
+#假设一段楼梯共 15 个台阶，小明一步最多能上 3 个台阶。编写程序计算小明上这段楼梯一共有多少种方法。要求给出递推法和递归法两种代码。
+#递推法
+__import__('functools').reduce(lambda s, _: s[1:] + [sum(s)], range(15), [0, 1, 2])[0]
+#递归法
+(lambda f: (lambda x: x(x))(lambda y: f(lambda a: y(y)(a))))(lambda f: lambda n: n if n < 3 else sum(map(f, range(n - 3, n))))(15)
