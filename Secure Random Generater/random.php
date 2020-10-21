d6454ae26c2c354df66af8ffaeb956580ae5d361
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-10-21 17:23:31 +0800

    Update random.php

--- Secure Random Generater/random.php
+++ Secure Random Generater/random.php
@@ -2,4 +2,4 @@
 $begin = $_GET['begin'] ?? 1;
 $end = $_GET['end'] ?? 6;
 $length = $_GET['length'] ?? 1;
-echo json_encode(array('data' => array_map(function () use (&$begin, $end){return random_int($begin, $end);}, range(1, $length))));
+echo json_encode(array('data' => array_map(function () use (&$begin, &$end){return random_int($begin, $end);}, range(1, $length))));
