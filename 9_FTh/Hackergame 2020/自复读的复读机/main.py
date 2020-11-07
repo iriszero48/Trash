71488b1cd8b3567de07a7b66b498948896e16c98
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-11-07 16:10:28 +0800

    Create main.py

---
+++ 9_FTh/Hackergame 2020/自复读的复读机/main.py
@@ -0,0 +1,2 @@
+_='_=%r;print((_%%_)[::-1],end="")';print((_%_)[::-1],end="")
+_=b'_=%r;print(__import__("hashlib").sha256(_%%_).hexdigest(),end="")';print(__import__("hashlib").sha256(_%_).hexdigest(),end="")
