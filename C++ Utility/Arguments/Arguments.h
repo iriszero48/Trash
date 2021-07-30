db85b602e1c55ffa91874c9e9597a23238f1b4de
iriszero <31085979+iriszero48@users.noreply.github.com>
2021-07-30 17:53:53 +0800

    Update Arguments.h

--- C++ Utility/Arguments/Arguments.h
+++ C++ Utility/Arguments/Arguments.h
@@ -6,6 +6,7 @@
 #include <functional>
 #include <optional>
 #include <sstream>
+#include <typeindex>
 #include <variant>
 #include <unordered_map>
 #include <vector>
@@ -27,17 +28,17 @@ std::string __Arguments_Combine__(Args&&... args)
 
 namespace ArgumentsParse
 {
-	constexpr auto Version = "1.0.1";
+	constexpr auto Version = "1.1.0";
 
 	using ArgLengthType = std::uint8_t;
-	
+
+	template<ArgLengthType Len> struct SetValueTypeImp { using Type = std::vector<std::string_view>; };
+	template<> struct SetValueTypeImp<1> { using Type = std::string_view; };
+	template<> struct SetValueTypeImp<0> { using Type = std::nullopt_t; };
+
 	class IArgument
 	{
 	public:
-		template<ArgLengthType Len> struct SetValueTypeImp { using Type = std::vector<std::string_view>; };
-		template<> struct SetValueTypeImp<1> { using Type = std::string_view; };
-		template<> struct SetValueTypeImp<0> { using Type = std::nullopt_t; };
-		
 		using SetValueType = std::variant<SetValueTypeImp<0>::Type, SetValueTypeImp<1>::Type, SetValueTypeImp<2>::Type>;
 
 		IArgument() = default;
@@ -46,15 +47,14 @@ namespace ArgumentsParse
 		IArgument(IArgument&& iArgument) = default;
 		IArgument& operator=(const IArgument& iArgument) = default;
 		IArgument& operator=(IArgument&& iArgument) = default;
-		
+
 		virtual void Set(const SetValueType& value) = 0;
-		virtual operator std::string() const = 0;
-		virtual std::any Get() = 0;
-		virtual std::string GetName() = 0;
-		virtual std::string GetDesc() = 0;
-		virtual ArgLengthType GetArgLength() = 0;
+		virtual std::any Get() const = 0;
+		virtual std::string GetName() const = 0;
+		virtual std::string GetDesc() const = 0;
+		virtual ArgLengthType GetArgLength() const = 0;
 	};
-	
+
 	template <typename T = std::string, ArgLengthType ArgLength = 1>
 	class Argument final : public IArgument
 	{
@@ -76,7 +76,7 @@ namespace ArgumentsParse
 			std::string name,
 			std::string desc,
 			ValueTypeOpt defaultValue,
-			ConvertFuncType convert = DefaultConvert):
+			ConvertFuncType convert = DefaultConvert) :
 			name(std::move(name)),
 			desc(std::move(desc)),
 			val(std::move(defaultValue)),
@@ -104,15 +104,13 @@ namespace ArgumentsParse
 			}
 		}
 
-		[[nodiscard]] std::any Get() override { return val; }
+		[[nodiscard]] std::any Get() const override { return val; }
 
-		[[nodiscard]] std::string GetName() override { return name; }
+		[[nodiscard]] std::string GetName() const override { return name; }
 
-		[[nodiscard]] std::string GetDesc() override { return desc; }
+		[[nodiscard]] std::string GetDesc() const override { return desc; }
 
-		[[nodiscard]] operator std::string() const override { return name; }
-
-		ArgLengthType GetArgLength() override { return ArgLength; }
+		ArgLengthType GetArgLength() const override { return ArgLength; }
 
 		static ConvertResult DefaultConvert(ConvertFuncParamType value) { return { ValueTypeOpt(value), {} }; }
 
@@ -133,30 +131,47 @@ namespace ArgumentsParse
 		{
 			if (args.find(arg.GetName()) != args.end())
 			{
-				__Arguments_ThrowEx__(args.at(arg)->GetName(), ": exists");
+				__Arguments_ThrowEx__(args.at(arg.GetName())->GetName(), ": exists");
 			}
 			args[arg.GetName()] = &arg;
 		}
 
 		std::string GetDesc();
 
-		template <typename T = std::string>
-		T Value(const std::string& arg)
+		template<typename T, typename F>
+		std::pair<const std::type_index, std::function<std::string(std::any const&)>> GetValuesDescConverter(F const& f)
+		{
+			using OpT = std::optional<T>;
+			return {
+				std::type_index(typeid(OpT)),
+				[g = f](std::any const& a)
+				{
+					return g(*std::any_cast<OpT const&>(a));
+				}
+			};
+		}
+
+		std::string GetValuesDesc(const std::unordered_map<std::type_index, std::function<std::string(std::any const&)>>& map);
+
+		template <typename T, ArgLengthType Len>
+		T Value(const Argument<T, Len>& arg) const
 		{
 			try
 			{
-				return Get<T>(arg).value();
+				return Get<T, Len>(arg).value();
 			}
 			catch (const std::exception& e)
 			{
-				__Arguments_ThrowEx__(args.at(arg)->GetName(), ": ", e.what());
+				__Arguments_ThrowEx__(args.at(arg.GetName())->GetName(), ": ", e.what());
 			}
 		}
 
-		template <typename T = std::string>
-		std::optional<T> Get(const std::string& arg)
+		template <typename T, ArgLengthType Len>
+		std::optional<T> Get(const Argument<T, Len>& arg) const
 		{
-			return std::any_cast<std::optional<T>>(args.at(arg)->Get());
+			return args.at(arg.GetName())->Get().type() == typeid(std::nullopt) ?
+				std::nullopt :
+				std::any_cast<std::optional<T>>(args.at(arg.GetName())->Get());
 		}
 
 		IArgument* operator[](const std::string& arg);
@@ -165,9 +180,9 @@ namespace ArgumentsParse
 		std::unordered_map<std::string, IArgument*> args;
 	};
 
-#define ArgumentOption(option, ...)\
+#define ArgumentOptionHpp(option, ...)\
 	enum class option { __VA_ARGS__ };\
-	const auto __##option##_map__ = (([&, i = 0](std::string str) mutable\
+	static auto __##option##_map__ = (([i = 0](std::string str) mutable\
 	{\
 		str.erase(std::remove(str.begin(), str.end(), ' '), str.end());\
 		std::unordered_map<option, const std::string> res{};\
@@ -176,9 +191,14 @@ namespace ArgumentsParse
 		while (std::getline(stringStream, item, ',')) res.emplace(static_cast<option>(i++), item);\
 		return res;\
 	})(#__VA_ARGS__));\
+	std::string ToString(const option& in);\
+	std::optional<option> To##option(const std::string& in);\
+	std::string option##Desc(const std::string& defaultValue = "");
+
+#define ArgumentOptionCpp(option)\
 	std::string ToString(const option& in) { return __##option##_map__.at(in); }\
-	option To##option(const std::string& in) { for (const auto& [k, v] : __##option##_map__) if (v == in) return k; }\
-	std::string option##Desc(const std::string& defaultValue = "")\
+	std::optional<option> To##option(const std::string& in) { for (const auto& [k, v] : __##option##_map__) if (v == in) return k; return std::nullopt; }\
+	std::string option##Desc(const std::string& defaultValue)\
 	{\
 		std::ostringstream oss{};\
 		oss << "[";\
@@ -196,6 +216,10 @@ namespace ArgumentsParse
 		res[res.length() - 1] = ']';\
 		return res;\
 	}
+
+#define ArgumentOption(option, ...)\
+	ArgumentOptionHpp(option, __VA_ARGS__)\
+	ArgumentOptionCpp(option)
 }
 
 #undef __Arguments_ToStringFunc__
