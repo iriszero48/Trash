34dc3206a181ae0ef020f185bbfdd71d77ddcba1
iriszero <31085979+iriszero48@users.noreply.github.com>
2021-11-05 09:40:57 +0800

    Update Recursive.java

--- Solutions by Programming Task/Fibonacci sequence/Java/Recursive.java
+++ Solutions by Programming Task/Fibonacci sequence/Java/Recursive.java
@@ -1,16 +1,13 @@
 import java.math.BigInteger;
 
-public class Main
-{
-    private static BigInteger fib(BigInteger x)
-    {
+public class Main {
+    private static BigInteger fib(BigInteger x) {
         return x.compareTo(BigInteger.valueOf(2)) < 0
                 ? x
                 : fib(x.subtract(BigInteger.valueOf(1))).add(fib(x.subtract(BigInteger.valueOf(2))));
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println(fib(BigInteger.valueOf(10)));
     }
 }
