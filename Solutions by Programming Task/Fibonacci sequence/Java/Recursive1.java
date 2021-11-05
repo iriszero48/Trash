9809aac1d1664d514ec948b8317d549e8718ed36
iriszero <31085979+iriszero48@users.noreply.github.com>
2021-11-05 09:41:47 +0800

    Update Recursive1.java

--- Solutions by Programming Task/Fibonacci sequence/Java/Recursive1.java
+++ Solutions by Programming Task/Fibonacci sequence/Java/Recursive1.java
@@ -2,16 +2,15 @@ public class Main
 {
     private static int fib(int x)
     {
-        return switch (x)
-        {
-            case 0 -> 0;
-            case 1 -> 1;
-            default -> fib(x - 1) + fib(x - 2);
-        };
+        return
+                switch (x) {
+                    case 0 -> 0;
+                    case 1 -> 1;
+                    default -> fib(x - 1) + fib(x - 2);
+                };
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println(fib(10));
     }
 }
